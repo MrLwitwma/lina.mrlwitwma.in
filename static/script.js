3ba8b3e4edd0419ec79641255f1d230edbadf6c0
MrLwitwma <147829440+MrLwitwma@users.noreply.github.com>
2024-06-20 10:48:48 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -32,13 +32,25 @@ document.addEventListener('DOMContentLoaded', () => {
             const userId = getOrCreateUserId(); // Get or create user ID
             displayMessage(`<b style="font-size:16px"> User </b><br>${escapeHTML(userMessage)}`, 'user');
             loading_message();
-            const response = await fetch('https://mrlwitwma.pythonanywhere.com/get_response', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: `user_id=${userId}&user_input=${encodeURIComponent(userMessage)}`
-            });
-            const responseData = await response.json();
-            displayMessage(responseData.response, 'ai'); // Allow HTML responses to work
+
+            if (userMessage && userMessage.startsWith('/generate')){
+                const response = await fetch('https://mrlwitwma.pythonanywhere.com/generate', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                    body: `user_id=${userId}&user_input=${encodeURIComponent(userMessage)}`
+                });
+                const responseData = await response.json();
+                displayMessage(responseData.response, 'ai', responseData.image); // Allow HTML responses to work
+            }
+            else{
+                const response = await fetch('https://mrlwitwma.pythonanywhere.com/get_response', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                    body: `user_id=${userId}&user_input=${encodeURIComponent(userMessage)}`
+                });
+                const responseData = await response.json();
+                displayMessage(responseData.response, 'ai'); // Allow HTML responses to work
+            }
             loaded_message();
         }
     }
@@ -53,7 +65,12 @@ document.addEventListener('DOMContentLoaded', () => {
         chatDisplay.scrollTop = chatDisplay.scrollHeight;
     }
 
-    function displayMessage(message, sender) {
+    function displayMessage(message, sender, image) {
+
+        if (typeof image === 'undefined') {
+            image = false;
+        }
+
         let class_id = generateUserId() + generateUserId() + generateUserId();
         const messageDiv = document.createElement('div');
         messageDiv.classList.add(sender);
@@ -65,10 +82,20 @@ document.addEventListener('DOMContentLoaded', () => {
                 return "<pre><code id='code'>" + codeContent + "</code></pre>";
             });
         }
-        messageDiv.innerHTML = message; // Allow HTML responses to work
+        else{
+            messageDiv.innerHTML += message; // Allow HTML responses to work
+            if (image) {
+                const imageDIV = document.createElement('img');
+                imageDIV.classList.add(sender);  // Assuming sender is a CSS class name
+                imageDIV.setAttribute('src', 'data:image/png;base64,' + image);  // Assuming the message is a base64 encoded PNG image
+                messageDiv.appendChild(imageDIV);  // Assuming messageDiv is the container where you want to append the image
+            }
+        }
+
         if (sender === 'ai'){
             messageDiv.innerHTML += `<div id="bottom-options"><span class="copy-button" title="Copy" data-code="${class_id}"><svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon-md"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3.5C10.8954 3.5 10 4.39543 10 5.5H14C14 4.39543 13.1046 3.5 12 3.5ZM8.53513 3.5C9.22675 2.3044 10.5194 1.5 12 1.5C13.4806 1.5 14.7733 2.3044 15.4649 3.5H17.25C18.9069 3.5 20.25 4.84315 20.25 6.5V18.5C20.25 20.1569 19.1569 21.5 17.25 21.5H6.75C5.09315 21.5 3.75 20.1569 3.75 18.5V6.5C3.75 4.84315 5.09315 3.5 6.75 3.5H8.53513ZM8 5.5H6.75C6.19772 5.5 5.75 5.94772 5.75 6.5V18.5C5.75 19.0523 6.19772 19.5 6.75 19.5H17.25C18.0523 19.5 18.25 19.0523 18.25 18.5V6.5C18.25 5.94772 17.8023 5.5 17.25 5.5H16C16 6.60457 15.1046 7.5 14 7.5H10C8.89543 7.5 8 6.60457 8 5.5Z" fill="currentColor"></path></svg></span>`;
         }
+        
         chatDisplay.appendChild(messageDiv);
 
         // Scroll to the bottom after adding a new message
